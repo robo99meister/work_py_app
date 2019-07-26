14e58c653937eeaea2c21d6635e13df506cacdc5
robo99meister <53320224+robo99meister@users.noreply.github.com>
2019-07-26 14:40:23 +0900

    add comment
    コメントを追加

--- 02_ssh_get_log.py
+++ 02_ssh_get_log.py
@@ -1,3 +1,4 @@
+#ログファイルを取得し、C:\pythonフォルダに保存する
 import paramiko
 
 fw = open('C:\python\JobExecute.1.txt','w')
@@ -6,7 +7,9 @@ client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.WarningPolicy())
 client.connect('192.168.0.10', username='root', password='toshibatec1048')
 
+#tdtr関連のログのみ抽出
 stdin, stdout, stderr = client.exec_command('grep tdtr /opt/work/log/al/JobExecute.1.txt')
+#1行ずつ保存
 for line in stdout:
     fw.writelines(line)
 
--- 04ssh_reboot.py
+++ 04ssh_reboot.py
@@ -1,9 +1,11 @@
+#ログを消去して再起動
 import paramiko
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.WarningPolicy())
 client.connect('192.168.0.10', username='root', password='toshibatec1048')
 
+#ログを削除、ログが5MBを超えると、ファイル名が変更されるため、毎回削除する。
 client.exec_command('rm /opt/work/log/al/JobExecute.1.txt')
 client.exec_command('reboot')
 
