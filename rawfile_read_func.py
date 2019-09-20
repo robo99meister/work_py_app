cd176250027604e2f64d5f7be758bb26a8983e7a
robo99meister <knmailphone@gmail.com>
2019-09-20 09:43:01 +0900

    change_common_id

--- rawfile_read_func.py
+++ rawfile_read_func.py
@@ -1,7 +1,7 @@
 #read raw file
 import struct
 
-fid = open("C:\python\libtdtr_toppage_20190731102500_reuse_1652x2338.raw", 'rb')   # 'r'は読み込み専用、'b'はバイナリモードで読み込みを表す
+fid = open("C:\python\libtdtr_toppage_20190820141912_reuse_1652x2338.raw", 'rb')   # 'r'は読み込み専用、'b'はバイナリモードで読み込みを表す
 
 a = struct.unpack('B', fid.read(1))   # 'f'はfloat32の場合、doubleは'd', int8は'b', int16は'h'
 print(a)
